d34015317ea6bb342af86f34bea96ddd5a873604
Christiaan Meyer <christiaan.meyer@entelect.co.za>
2023-07-15 12:35:06 +0200

    initial commit

---
+++ Persistence/Migrations/20230715100310_DBFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tite",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Tite");
+        }
+    }
+}
